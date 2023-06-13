fa482f80c762fd7a172864a5a52a28b97dfe70ac
mitunda <peterbrian28@gmail.com>
2023-06-13 12:58:33 +0300

    update

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function distanceFromHqInBlocks(location) {
     return Math.abs(location - hqLocation);
   }
   console.log(distanceFromHqInBlocks(50));
-  function distanceFromHqInFeet(block) {
-    return distanceFromHqInBlocks(block) * 264;
+
+  function distanceFromHqInFeet(block, ft) {
+    return distanceFromHqInBlocks(block) * 264; 
+    // the * is to multiply by the number of blocks
   }
-  
+
   function distanceTravelledInFeet(startBlock, endBlock) {
     return Math.abs(startBlock - endBlock) * 264;
+    // the mathabs returns a positive because distance cant be negative 
   }
   
   function calculatesFarePrice(startBlock, endBlock) {
